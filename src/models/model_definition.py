3a7eeec91ef95320d990b24859b7f58d69c66673
SagaRut <sagakudungur@gmail.com>
2024-05-03 21:10:02 +0200

    Moved the params

--- src/models/model_definition.py
+++ src/models/model_definition.py
@@ -4,6 +4,17 @@ from model_params import params
 from joblib import load
 
 def getModel():
+    params = {'loss_function': 'binary_crossentropy',
+              'optimizer': 'adam',
+              'sequence_length': 200,
+              'batch_train': 5000,
+              'batch_test': 5000,
+              'categories': ['phishing', 'legitimate'],
+              'char_index': None,
+              'epoch': 30,
+              'embedding_dimension': 50,
+              'dataset_dir': "../dataset/small_dataset/"}
+
     model = Sequential()
     char_index = load('../../output/char_index.joblib')
     voc_size = len(char_index.keys())
